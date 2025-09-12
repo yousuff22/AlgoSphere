d212f66d5f0736ba94d105170fc4ac094263db30
yousuff22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2025-09-12 20:21:51 +0530

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Allow external connections
+    port: 5173,
+    hmr: {
+      port: 5173,
+      host: "143.110.176.130", // Use your server's public IP
+    },
+  },
+});
